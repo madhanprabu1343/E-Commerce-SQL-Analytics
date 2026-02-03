281e3a9b15753e0383e1a4d6fa25f2d3e9246a08
madhanprabu1343 <madhanprabu1343@gmail.com>
2026-02-03 17:22:19 +0530

    Add customer order frequency analysis

--- sql/02_joins_and_metrics.sql
+++ sql/02_joins_and_metrics.sql
@@ -7,3 +7,13 @@ JOIN orders o
     ON c.customer_id = o.customer_id
 GROUP BY c.city
 ORDER BY total_revenue DESC;
+
+-- Customers by number of orders
+SELECT 
+    c.name,
+    COUNT(o.order_id) AS total_orders
+FROM customers c
+LEFT JOIN orders o
+    ON c.customer_id = o.customer_id
+GROUP BY c.name
+ORDER BY total_orders DESC;
